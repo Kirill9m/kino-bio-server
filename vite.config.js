0b4b3414ac372c192bcd6c336936f491702c62df
Kirill <kirillsavorin@gmail.com>
2025-01-15 22:12:05 +0100

    Repo cloned

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/kino-bio-projekt/', // Replace 'my-vite-app' with your repository name
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        aboutUs: './about-us.html',
+        movies: './movies.html',
+      },
+    },
+  },
+});
