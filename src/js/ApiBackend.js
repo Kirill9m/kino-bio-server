52838cc6763215b024fbeb50073f8cb0ebc8547b
Kirill <kirillsavorin@gmail.com>
2025-01-18 00:43:51 +0100

    Jest installed and some easy test added

--- src/js/ApiBackend.js
+++ src/js/ApiBackend.js
@@ -1,15 +1,14 @@
 export default class ApiBackend {
-    constructor(apiUrl) {
-        this.apiUrl = apiUrl;
-    }
+  constructor(apiUrl) {
+    this.apiUrl = apiUrl;
+  }
 
-    async loadAllFilms() {
-        const response = await fetch(this.apiUrl + '/movies');
-        if (!response.ok) {
-            throw new Error(`Failed to fetch movies: ${response.statusText}`);
-        }
-        const payload = await response.json();
-        console.log(payload)
-        return payload;
+  async loadAllFilms() {
+    const response = await fetch(this.apiUrl + '/movies');
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movies: ${response.statusText}`);
     }
+    const payload = await response.json();
+    return payload;
+  }
 }
