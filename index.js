1d1ce9c7fd9af551e90fdb3d966674d3dee5053e
Kirill <kirillsavorin@gmail.com>
2025-01-18 14:00:53 +0100

    SOC

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
 import express from 'express';
-import fs from 'fs/promises';
+import renderPage from './lib/renderPage.js';
 
 const app = express();
-
-const MENU = [
-  {
-    label: 'Home',
-    link: '/',
-  },
-  {
-    label: 'Movies',
-    link: '/movies',
-  },
-  {
-    label: 'About',
-    link: '/about-us',
-  },
-];
-async function renderPage(response, page) {
-  const contentBuf = await fs.readFile(`./content/${page}.html`);
-  const contentText = contentBuf.toString();
-
-  const templateBuf = await fs.readFile('./templates/main.html');
-  const templateText = templateBuf.toString();
-
-  const menuItems = MENU.map((item) => {
-    return `<li class="header__nav-item"><a href="${item.link}" class="header__nav-link" aria-label="Go to Biljetter">${item.label}</a>
-          </li>`;
-  });
-
-  const menuItemsMobile = MENU.map((item) => {
-    return `<li class="hamburger__list"><a href="${item.link}">${item.label}</a></li>`;
-  });
-
-  const menuText = menuItems.join('\n');
-  const menuTextMobile = menuItemsMobile.join('\n');
-
-  const outputHtml = templateText
-    .replace('@mainOne', contentText)
-    .replace('@menu', menuText)
-    .replace('@menuMobile', menuTextMobile);
-
-  response.send(outputHtml);
-}
 
 app.get('/', (request, response) => {
   renderPage(response, 'index');
