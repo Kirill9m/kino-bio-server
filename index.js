1a824a612f6b1ddaf29a719120021b8f54922a35
Kirill <kirillsavorin@gmail.com>
2025-01-18 13:43:38 +0100

    Dynamic menu for mobile

--- index.js
+++ index.js
@@ -24,14 +24,22 @@ async function renderPage(response, page) {
   const templateBuf = await fs.readFile('./templates/main.html');
   const templateText = templateBuf.toString();
 
-  const htmlItems = MENU.map((item) => {
+  const menuItems = MENU.map((item) => {
     return `<li class="header__nav-item"><a href="${item.link}" class="header__nav-link" aria-label="Go to Biljetter">${item.label}</a>
           </li>`;
   });
 
-  const menuText = htmlItems.join('\n');
+  const menuItemsMobile = MENU.map((item) => {
+    return `<li class="hamburger__list"><a href="${item.link}">${item.label}</a></li>`;
+  });
 
-  const outputHtml = templateText.replace('@mainOne', contentText).replace('@menu', menuText);
+  const menuText = menuItems.join('\n');
+  const menuTextMobile = menuItemsMobile.join('\n');
+
+  const outputHtml = templateText
+    .replace('@mainOne', contentText)
+    .replace('@menu', menuText)
+    .replace('@menuMobile', menuTextMobile);
 
   response.send(outputHtml);
 }
