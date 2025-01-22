76145011a65421cc2c6698b43982281ef66ed142
Kirill <kirillsavorin@gmail.com>
2025-01-22 22:11:31 +0100

    Moving movies page on server rendering!

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import nunjucks from 'nunjucks';
 import renderPage from './lib/renderPage.js';
 import ApiBackend from './src/js/ApiBackend.js';
 import { marked } from 'marked';
+import { loadMovie, loadMovies } from './src/backend/moviesLoad.js';
 
 const PORT = process.env.PORT || 5080;
 const app = express();
-const backend = new ApiBackend('https://plankton-app-xhkom.ondigitalocean.app/api');
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -20,57 +20,28 @@ app.get('/', (request, response) => {
   renderPage(response, 'pages/index', 'Home');
 });
 
-app.get('/movies', (request, response) => {
-  renderPage(response, 'pages/movies', 'Movies');
+app.get('/movies', async (request, response) => {
+  const movies = await loadMovies();
+  response.render('pages/movies', { movies });
+  // renderPage(response, 'pages/movies', 'Movies');
 });
 
 app.get('/about-us', (request, response) => {
   renderPage(response, 'pages/about-us', 'About');
 });
 
-app.get('/movies/api', async (request, response) => {
-  try {
-    const films = await backend.loadAllFilms();
-
-    const newFilms = [];
-    for (let i = 0; i < films.data.length; i++) {
-      const film = films.data[i];
-
-      const updatedFilm = {
-        id: film.id,
-        attributes: {
-          title: film.attributes.title,
-          imdbId: film.attributes.imdbId,
-          intro: marked.parse(film.attributes.intro),
-          image: film.attributes.image,
-          createdAt: film.attributes.createdAt,
-          updatedAt: film.attributes.updatedAt,
-          publishedAt: film.attributes.publishedAt,
-        },
-      };
-
-      newFilms.push(updatedFilm);
-    }
-
-    response.json({
-      data: newFilms,
-    });
-  } catch (error) {
-    response.status(500).json({ error: 'Failed to fetch movies' });
-  }
-});
-
 app.get('/movies/id/:id', async (request, response) => {
-  const filmId = request.params.id;
   try {
-    const film = await backend.loadFilmById(filmId);
-    const filmData = film.data.attributes;
-    const filmDataIntro = marked.parse(film.data.attributes.intro);
+    const movie = await loadMovie(req.params.movieId);
+    res.render('movie', { movie });
+    // const film = await backend.loadFilmById(filmId);
+    // const filmData = film.data.attributes;
+    // const filmDataIntro = marked.parse(film.data.attributes.intro);
 
-    renderPage(response, 'layout/movieLayout', filmData.title, {
-      film: filmData,
-      filmDataIntro,
-    });
+    // renderPage(response, 'layout/movieLayout', filmData.title, {
+    //   film: filmData,
+    //   filmDataIntro,
+    // });
   } catch (error) {
     response.status(404);
     renderPage(response, 'pages/404', 'Error 404', { title: '404 Not Found' });
