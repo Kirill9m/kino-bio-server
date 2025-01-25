e9b13bd438429e210a08175c19ed4642b3167514
Kirill <kirillsavorin@gmail.com>
2025-01-25 15:29:51 +0100

    New test film id and 404 page

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -2,7 +2,7 @@ import { expect, test } from '@jest/globals';
 import app from "../src/backend/app.js";
 import request from 'supertest';
 
-test('Movie page shows list of films', async () => {
+test('Movies page shows list of films', async () => {
   const response = await request(app)
     .get('/movies')
     .expect('Content-Type', /html/)
@@ -11,4 +11,20 @@ test('Movie page shows list of films', async () => {
     expect(response.text).toMatch('Encanto');
     expect(response.text).toMatch('Forrest Gump');
     expect(response.text).toMatch('Training Day');
+});
+
+test('Movie page shows a film', async () => {
+  const response = await request(app)
+    .get('/movies/id/1')
+    .expect('Content-Type', /html/)
+    .expect(200);
+    expect(response.text).toMatch('eachMovie__title');
+});
+
+test('Movie page shows 404 page', async () => {
+  const response = await request(app)
+    .get('/movies/id/10000')
+    .expect('Content-Type', /html/)
+    .expect(404);
+    expect(response.text).toMatch('Det finns inget sådant ID eller film!');
 });
